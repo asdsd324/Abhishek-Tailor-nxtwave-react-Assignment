431b0931152d6610c1edbbc9bd8adafe29ad2cf6
vedpra55 <vedna400@gmail.com>
2022-10-19 11:14:15 +0530

    change

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,13 @@ export default function Header() {
     <div className="flex justify-between items-center px-20 py-5 bg-white  border-b">
       <div>
         <Link to="/">
-          <img src="./abhishek-tailor-nxtwave-react-assignment/img/logo.png" />
+          <img src="/img/logo.png" />
         </Link>
       </div>
       <div className="flex gap-x-3">
         <div className="px-6 py-2 bg-blue-600 text-white rounded-md">
           <Link to="/createItem">
-            <button>ADDS</button>
+            <button>ADD</button>
           </Link>
         </div>
         <img className=" rounded-full" src="/img/image.png" />
