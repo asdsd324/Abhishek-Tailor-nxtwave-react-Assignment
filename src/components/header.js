f5a59aed201c11f111192ae3c93a856fd5fd0925
vedpra55 <vedna400@gmail.com>
2022-10-19 10:57:31 +0530

    change

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,13 @@ export default function Header() {
     <div className="flex justify-between items-center px-20 py-5 bg-white  border-b">
       <div>
         <Link to="/">
-          <img src="/img/logo.png" />
+          <img src="./abhishek-tailor-nxtwave-react-assignment/img/logo.png" />
         </Link>
       </div>
       <div className="flex gap-x-3">
         <div className="px-6 py-2 bg-blue-600 text-white rounded-md">
           <Link to="/createItem">
-            <button>ADD</button>
+            <button>ADDS</button>
           </Link>
         </div>
         <img className=" rounded-full" src="/img/image.png" />
--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter
-      basename={
-        "https://asdsd324.github.io/abhishek-tailor-nxtwave-react-assignment"
-      }
-    >
+    <BrowserRouter basename={"/abhishek-tailor-nxtwave-react-assignment"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
